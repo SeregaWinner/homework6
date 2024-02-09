396710d70c59ceaeca4126d9791c8bc4b577a6b2
user <Seregawinner92@gmail.com>
2024-02-09 14:50:50 +0300

    fixing homework cycles

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("Задание 1");
-        for (int i = 0; i < 10; i++){
+        for (int i = 1; i <= 10; i++){
             System.out.println("Итерация цикла " + i);}
 
         System.out.println("Задание 2");
-        for (int x = 10; x >= 0; x--){
+        for (int x = 10; x >= 1; x--){
             System.out.println("Итерация цикла " + x);}
 
         System.out.println("Задание 3");
@@ -19,15 +19,12 @@ public class Main {
             System.out.println("Итерация цикла " + x);}
 
         System.out.println("Задание 5");
-        for (int y = 1904; y <= 2096; y = y + 4) {
-            System.out.println(y + " год являеться високосным." );}
+        for (int year = 1904; year <= 2096; year = year + 4) {
+            System.out.println(year + " год являеться високосным." );}
 
         System.out.println("Задание 6");
-        int initial = 7;
-        int subsequent = 0;
         for (int i = 7; i <= 98; i = i + 7) {
-            subsequent = subsequent + initial;
-            System.out.println(subsequent);}
+            System.out.println(i);}
 
         System.out.println("Задание 7");
         for (int k = 1; k <= 512; k = k * 2) {
@@ -37,9 +34,9 @@ public class Main {
         System.out.println("Здание 8");
         int postponed = 29000;
         int accumulation = 0;
-        for (int i = 0; i <= 12;  i ++) {
+        for (int month = 1; month <= 12;  month ++) {
             accumulation = accumulation + postponed;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + accumulation + " рублей");}
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + accumulation + " рублей");}
 
         System.out.println("Здание 9");
         int bank = 29000;
